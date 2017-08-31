3f1c6938120463df19387195cb34135601f3a511
Sam Nixa <spiznasm@gmail.com>
2017-08-31 07:37:09 -0500

    added partials

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,5 @@
 module ApplicationHelper
-<<<<<<< HEAD
-=======
 
->>>>>>> cc7e8752c4d87909390cf73e85ca31133e0fbe7d
   def full_title(page_title = '')
     base_title = "Ruby on Rails Tutorial Sample App"
     if page_title.empty?
@@ -11,8 +8,4 @@ module ApplicationHelper
       page_title + " | " + base_title
     end
   end
-<<<<<<< HEAD
-=======
-  
->>>>>>> cc7e8752c4d87909390cf73e85ca31133e0fbe7d
 end
