fd9f42c5322a48ea07d3ce2af0d963aece591182
Sam Nixa <spiznasm@gmail.com>
2017-11-10 18:03:46 -0600

    Finish user edit, update, index and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -16,6 +16,11 @@ module SessionsHelper
     cookies.delete(:remember_token)
   end
 
+  # Returns true if given user is the current user.
+  def current_user?(user)
+    user == current_user
+  end
+
   def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
@@ -36,6 +41,17 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+
+  #Redirect to stored location or to default
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  #Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 
 end
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -4,17 +4,43 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
-  
-  test "layout links" do
+
+  def setup
+    @user = users(:michael)
+  end
+
+  test "layout links when not logged in" do
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", users_path, count: 0
     get contact_path
     assert_select "title", full_title("Contact")
     get signup_path
     assert_select "title", full_title("Sign up")
+    get help_path
+    assert_select "title", full_title("Help")
+    get about_path
+    assert_select "title", full_title("About")
+    get login_path
+    assert_select "title", full_title("Log in")
+  end
+
+  test "layout links for logged in user" do
+    log_in_as(@user)
+    get user_path(@user)
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+    get users_path
+    assert_select "title", full_title("All users")
+    get user_path(@user)
+    assert_select "title", full_title(@user.name)
+    get edit_user_path(@user)
+    assert_select "title", full_title("Edit user")
   end
 end
