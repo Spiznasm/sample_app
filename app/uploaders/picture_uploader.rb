f50497961f7845ad5203f289f5e8f47788240436
Sam Nixa <spiznasm@gmail.com>
2017-12-26 16:43:16 -0600

    Configured pictures to use Cloudinary instead of AWS

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,24 +1,34 @@
 class PictureUploader < CarrierWave::Uploader::Base
-  include CarrierWave::MiniMagick
-  process resize_to_limit: [400,400]
+  include Cloudinary::CarrierWave
+
+  process :convert => 'jpg'
+  process :tags => ['post_picture']
+  
+  version :standard do
+    process :resize_to_fill => [400,400, :north]
+  end
+
+  version :thumbnail do
+    resize_to_fit(50,50)
+  end
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  if Rails.env.production?
-    storage :fog
-  else
-    storage :file
-  end
+  # if Rails.env.production?
+  #   storage :fog
+  # else
+  #   storage :file
+  # end
   
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
+  # def store_dir
+  #   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  # end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
